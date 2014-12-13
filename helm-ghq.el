e4f6764c09ab97194080f7d728ab530dcaec3e24
Takashi Masuda <masutaka.net@gmail.com>
2014-12-13 14:57:59 +0900

    Fix required helm version to 1.6.5

--- helm-ghq.el
+++ helm-ghq.el
@@ -5,7 +5,7 @@
 ;; Author: Takashi Masuda <masutaka.net@gmail.com>
 ;; URL: https://github.com/masutaka/emacs-helm-ghq
 ;; Version: 1.4.0
-;; Package-Requires: ((helm "1.6.2"))
+;; Package-Requires: ((helm "1.6.5"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
