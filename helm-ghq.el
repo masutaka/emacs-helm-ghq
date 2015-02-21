c1da607d68f2dfcba66fc1c23ec4bbce207e22b5
Takashi Masuda <masutaka.net@gmail.com>
2015-02-21 15:08:48 +0900

    Bump version 1.4.2

--- helm-ghq.el
+++ helm-ghq.el
@@ -4,7 +4,7 @@
 
 ;; Author: Takashi Masuda <masutaka.net@gmail.com>
 ;; URL: https://github.com/masutaka/emacs-helm-ghq
-;; Version: 1.4.1
+;; Version: 1.4.2
 ;; Package-Requires: ((helm "1.6.5"))
 
 ;; This program is free software; you can redistribute it and/or modify
