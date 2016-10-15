231a773136a438acfe96997c28b7a1d6a0b53a4b
Takashi Masuda <masutaka.net@gmail.com>
2016-10-15 16:14:19 +0900

    Update package requirement

--- helm-ghq.el
+++ helm-ghq.el
@@ -5,7 +5,7 @@
 ;; Author: Takashi Masuda <masutaka.net@gmail.com>
 ;; URL: https://github.com/masutaka/emacs-helm-ghq
 ;; Version: 1.6.0
-;; Package-Requires: ((helm "1.8.0"))
+;; Package-Requires: ((helm "2.2.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
